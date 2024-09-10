89f154905d06708e8fe494e47954e93993a1a310
Agustín Schulze <agsp3rs3o@gmail.com>
2024-09-10 13:47:28 -0300

    Adding interfaces

--- tasks/src/main/java/com/hexagonal/tasks/infrastructure/repositories/JpaTaskRepository.java
+++ tasks/src/main/java/com/hexagonal/tasks/infrastructure/repositories/JpaTaskRepository.java
@@ -1,4 +1,11 @@
 package com.hexagonal.tasks.infrastructure.repositories;
 
-public interface JpaTaskRepository {
+import com.hexagonal.tasks.infrastructure.entities.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaTaskRepository extends JpaRepository<TaskEntity, Long> {
+
+
+
+
 }
--- tasks/src/main/java/com/hexagonal/tasks/infrastructure/repositories/JpaTaskRepositoryAdapter.java
+++ tasks/src/main/java/com/hexagonal/tasks/infrastructure/repositories/JpaTaskRepositoryAdapter.java
@@ -1,4 +1,41 @@
 package com.hexagonal.tasks.infrastructure.repositories;
 
-public interface JpaTaskRepositoryAdapter {
+import com.hexagonal.tasks.domain.models.Task;
+import com.hexagonal.tasks.domain.ports.out.TaskRepositoryPort;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaTaskRepositoryAdapter implements TaskRepositoryPort {
+
+    private final JpaTaskRepository jpaTaskRepository;
+
+    public JpaTaskRepositoryAdapter(JpaTaskRepository jpaTaskRepository) {
+        this.jpaTaskRepository = jpaTaskRepository;
+    }
+
+    @Override
+    public Task save(Task task) {
+        return null;
+    }
+
+    @Override
+    public Optional<Task> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Task> update(Task task) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
 }
