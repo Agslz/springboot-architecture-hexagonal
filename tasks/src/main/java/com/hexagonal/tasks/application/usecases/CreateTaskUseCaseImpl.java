f14340c13b74f3efe159c303c515c6f4f6790ad7
Agustín Schulze <agsp3rs3o@gmail.com>
2024-09-10 10:26:29 -0300

    Structuring folders and classes

---
+++ tasks/src/main/java/com/hexagonal/tasks/application/usecases/CreateTaskUseCaseImpl.java
@@ -0,0 +1,12 @@
+package com.hexagonal.tasks.application.usecases;
+
+import com.hexagonal.tasks.domain.models.Task;
+import com.hexagonal.tasks.domain.ports.in.CreateTaskUseCase;
+
+public class CreateTaskUseCaseImpl implements CreateTaskUseCase {
+
+    @Override
+    public Task createTask(Task task) {
+        return null;
+    }
+}
